df506bac33e437b04cdc5d14d2b3f3f80a8a6f51
keyp0s <aidentubman@gmail.com>
2022-02-22 19:22:16 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# civicia-portal-timetable-scraper
+A python bot to scrape the timetable of the Igloo Portal  
+Displays as .json format
---
+++ timetable-scraper.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+import time
+import info
+import json
+
+class scraper:
+
+    USERNAME_ID = 'iwpSidebarPortlet|-1|null|tbUsername'
+    PASSWORD_ID = 'iwpSidebarPortlet|-1|null|tbPassword'
+    TABLE_CLASS = 'iwpTimetableGrid'
+
+    def __init__(self, username, password, directory):
+        self.username = username
+        self.password = password
+        self.directory = directory
+
+    def get_timetable(self):
+        chrome_service = Service(self.directory)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+        driver.get('https://portal.stpiusx.nsw.edu.au/igloo/portal/')
+
+        def load_status(element_type,id_name):
+            element_present = EC.presence_of_element_located((element_type, id_name))
+            WebDriverWait(driver, 10).until(element_present)
+
+        try:
+            load_status(By.ID, scraper.USERNAME_ID)
+
+            username = driver.find_element(By.ID, scraper.USERNAME_ID)
+            password = driver.find_element(By.ID, scraper.PASSWORD_ID)
+
+            username.send_keys(self.username)
+            password.send_keys(self.password)
+            password.send_keys(Keys.ENTER)
+
+            try:
+                load_status(By.CLASS_NAME, scraper.TABLE_CLASS)
+
+                timetable = driver.find_element(By.CLASS_NAME, scraper.TABLE_CLASS)
+                return (timetable.text)
+                driver.close()
+
+            except:
+                print("portal login timeout exception")
+                driver.close()
+        except:
+            print("portal page load timeout exception")
+            driver.close()
+
+INFO = scraper(info.creds()[0], info.creds()[1], info.creds()[2])
+
+table = scraper.get_timetable(INFO)
+
+print(table)
